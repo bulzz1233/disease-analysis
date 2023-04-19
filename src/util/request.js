489ba6b4e66f7c2ebc990784dd6e1fd7831f5a13
silong <bluzz1233@163.com>
2023-04-19 10:54:31 +0800

    修复bug

--- src/util/request.js
+++ src/util/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 // import { MessageBox } from 'element-ui';
 
-axios.defaults.headers['Content-Type'] = 'text/plain';
+axios.defaults.headers['Content-Type'] = 'application/json';
+axios.defaults.headers['User-Agent'] = 'PostmanRuntime/7.26.10';
 const request = axios.create({
     baseURL: '/api',
     timeout: 100000,
